d63ae566222b585d31fd18a1ff2e3627e28b7297
Steve <steve@maraspin.net>
2014-05-19 02:48:48 +0200

    Detail fetching data from MongoDb

--- detail.php
+++ detail.php
@@ -4,44 +4,23 @@ include_once 'config.inc.php';
 
 $id = $_GET['id'];
 
-try {
+$start = microtime(true);
 
-  $db = new PDO($dsn , 'postgres', 'zf2');
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  $sql = "SELECT prodotto.*, macrocategoria.nome as macrocategoria, variante.nome as variante,
-          categoria.nome as categoria FROM prodotto join categoria on categoria.id = prodotto.categoria_id 
-          join macrocategoria on macrocategoria.id = categoria.macrocategoria_id 
-          join prodottovariante on prodotto.id = prodottovariante.id_prodotto 
-          join variante on prodottovariante.id_variante = variante.id
-          WHERE prodotto.id = ". $id ;
+$m = new \MongoClient(); // connect
+$db = $m->ecommerce;
+$collection = $db->prodotti;
 
-  $start = microtime(true);
-  
-  $st = $db->query($sql);
-  $row = $st->fetch();
-  $item = $row;
-  $varianti = $item['variante'];
-  while ($row = $st->fetch()) {
-    $varianti .= ', '.$row['variante'];
-  }
-  $item['variante'] = $varianti;
+$m_searchKey = array('id' => (int)$id);
+$as_prodotto = $collection->findOne($m_searchKey);
+
+foreach($as_prodotto as $s_attribute => $m_value) {
+    if (!is_array($m_value)) {
+        echo ucfirst($s_attribute) . ": " . $m_value . "\n<br />";
+    } 
 }
-  catch (PDOException $e) {
-    print $e->getMessage();
-}
-?>
-<h1>Scheda Prodotto: <?php echo $item['nome']; ?></h1>
-<p>Prezzo: <?php echo $item['prezzo']; ?></p>
-<p>Venduti: <?php echo $item['venduti']; ?></p>
-<p>Disp. Dal: <?php echo $item['dataarrivo']; ?></p>
-<p>Varianti: <?php echo $item['variante']; ?>
     
-<?php  
-    
-    $time_taken = microtime(true) - $start;
+$time_taken = microtime(true) - $start;
 ?>
-</p>
 
 <?php echo "Time taken: " . $time_taken; ?>
 
